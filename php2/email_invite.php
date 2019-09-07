b2340cf7addf2ca46ae5887a4bd1db0934601d78
Harshit Gangwar <harshit02gangwar@gmail.com>
2019-09-07 23:53:07 +0530

    email invite php file created

---
+++ php2/email_invite.php
@@ -0,0 +1,70 @@
+<?php
+    
+    // $message = "this is an email invite form ExpendiShare";
+    // $subject = "Invite";
+
+    // if(isset($_POST['send']))
+    // {
+    //     $e = $_POST['email'];
+    //     $s = $subject;
+    //     $m = $message;
+       
+    //     if( mail($e, $s, $m) )     
+    //         echo "mail sent";
+    //     else
+    //         echo "not sent";
+    // }
+    // else
+    //     echo"email not send";
+?>
+
+
+ <?php
+ 
+    include("connect.php");
+    error_reporting(0);
+    session_start();
+
+    $user_email = $_SESSION['email'];
+    $query = "SELECT * FROM user_info WHERE email='$user_email' "; 
+    $data = mysqli_query($conn, $query);
+    $result = mysqli_fetch_assoc($data);
+    $name = $result['name'];
+    $check = 0;
+
+    $message = "\n Join ExpendiShare now \n http://localhost/ExpendiShare/html/login.html \n and share your bills with ".$name." and others";
+    $subject = $name." wants to share bills with you on ExpendiShare";
+
+    if(isset($_POST['send']))
+    {
+        $e = $_POST['email'];
+        $s = $subject;
+        $m = $message;
+
+        if( mail($e, $s, $m) )          
+        {   
+            ?>
+            <script>window.alert('Mail Invite Sent')</script>
+            <?php
+        }
+        else
+        {
+            ?>
+            <script>window.alert('Could not Send Mail Invite')</script>
+            <?php
+        }
+        ?>       
+        <!-- redirecting to the dshboard -->
+        <META HTTP-EQUIV="Refresh" CONTENT="0; URL= ../html/dashboard.html" >
+        <?php
+    
+        }
+    else
+    {
+        echo "not sent";
+            ?>
+            <script>window.alert('Could not Send Mail Invite')</script>
+            <?php
+    }
+        
+?>
