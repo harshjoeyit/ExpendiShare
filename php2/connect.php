e5e94752953b477fbea11277edcc7ba76524ae79
amantibrewal310 <amantibrewal310@gmail.com>
2019-09-08 21:42:26 +0530

    Database:
    Password issue fix

--- php2/connect.php
+++ php2/connect.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "4004";
 $dbname = "ExpendiShare";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
