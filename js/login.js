b9def2b65d2f7b756471cb415b5e911cba6d4121
Harshit Gangwar <harshit02gangwar@gmail.com>
2019-09-12 11:23:23 +0530

    show password option added

--- js/login.js
+++ js/login.js
@@ -6,3 +6,17 @@ signUpButton.addEventListener('click', ()=> container.classList.add('right-panel
 signInButton.addEventListener('click', ()=> container.classList.remove('right-panel-active'));
 
 
+function myFunction() {
+  var x = document.getElementById("myInput");
+  var y = document.getElementById("myInput2");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+  if (y.type === "password") {
+    y.type = "text";
+  } else {
+    y.type = "password";
+  }
+}
