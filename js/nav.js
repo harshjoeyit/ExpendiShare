2c1b195bda040f05292df3550fcc2c12a7bf5c53
Harshit Gangwar <harshit02gangwar@gmail.com>
2019-09-12 10:44:51 +0530

    styles removed from js

--- js/nav.js
+++ js/nav.js
@@ -9,11 +9,3 @@ $('a.cta').hide().delay(1300).show(600);
 
 
 
-// add this in the css file
-
-$logo.css({
-    // 'text-decoration' : 'underline'
-    'border' : '3px solid #DB2955 ',
-    'border-radius' : '8px',
-    'color' : '#FFEDDF'
-});
