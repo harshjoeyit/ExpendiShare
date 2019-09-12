64c3b259541e8ce7e7cea092140c5581f4e95079
Harshit Gangwar <harshit02gangwar@gmail.com>
2019-09-12 10:55:08 +0530

    animation show improved

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,14 @@
 // $('ul').hide().slideDown(900);
 
+$li =  $('li');
+$li.hide();
+
 $logo = $('.logo');
 $logo.hide().delay(500);
 $logo.slideDown(800);
 
-$('a.cta').hide().delay(1300).show(600);
+//$li.hide().delay(1300).show(600);
+$('a.cta, li').hide().delay(1300).show(800);
 
 
 
