e2923419c413276f74d2ec811b54dd71626ebaa1
Harshit Gangwar <harshit02gangwar@gmail.com>
2019-09-14 18:25:42 +0530

    update features added

--- php2/update.php
+++ php2/update.php
@@ -4,15 +4,59 @@ session_start();
 include('connect.php');
 $user_email = $_SESSION['email'];
 
-if()
+if($_POST['Update'])
 {
-    $query = "SELECT * FROM users_info WHERE email = '$user_email' ";
-    $data = mysqli_query($conn, $query);
-    $result = mysqli_fetch_assoc($data);
+    // validation of the details is required here 
+    if($_POST['name'] != "")
+    {
+        $name = $_POST['name'];
+        $update_query = "UPDATE users_info SET name = '$name' WHERE email = '$user_email' ";
+        $data = mysqli_query($conn, $update_query);
 
-    $name = $result['name'];
-    $profile_pic = $result['profile_pic'];
+        if($data)
+            header('location: account.php');
+    
+        else    
+        {
+            ?>
+            <script> window.alert('details not updated') </script>
+            <?php
+            //header('location: account.php');
+        }
 
+    }  
+
+    if($_POST['email'] != "")
+    {
+        $email = $_POST['email'];
+        $update_query = "UPDATE users_info SET email = '$email' WHERE email = '$user_email' ";
+        $data = mysqli_query($conn, $update_query);
+
+        if($data)
+        {
+            // session email must be changed since it is the same user but the email for session has changed 
+            $_SESSION['email'] = $email;        
+            header('location: account.php');
+        }
+        else    
+        {
+            ?>
+            <script> window.alert('details not updated') </script>
+            <?php
+            header('location: account.php');
+        }
+    }  
+    
+    // change password functionality after verifying the old password 
+
+    if( $_POST['email'] == "" && $_POST['name'] == "" )
+    {
+        ?>
+        <script> window.alert('Nothing to Update') </script>
+        <!-- redirect here using META -->
+        <?php
+    }
+    
 }
 
 ?>
